51e771e761f9a02c20b861e2f0dc581f38c06007
Nguyen Ho <mickey171904@gmail.com>
2026-04-15 14:15:19 -0700

    1st version of ExerciseService.java

--- src/main/java/com/HealthApp/service/ExerciseService.java
+++ src/main/java/com/HealthApp/service/ExerciseService.java
@@ -1,7 +1,40 @@
 package com.HealthApp.service;
 
+import com.HealthApp.model.Exercise;
+import com.HealthApp.repo.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ExerciseService {
 
+    @Autowired
+    private ExerciseRepository repo;
+
+    public List<Exercise> getAllExercises() {
+        return repo.findAll();
+    }
+
+    public Exercise getExerciseById (Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Exercise not found"));
+    }
+
+    public void saveExercise (Exercise exercise) {
+        repo.save(exercise);
+    }
+
+    public void deleteExercise (Long id) {
+        repo.deleteById(id);
+    }
+
+    public long countCertificate() {
+        return repo.count();
+    }
+
+    public void deleteAllCertificate() {
+        repo.deleteAll();
+    }
 }
