0100eecf0f09f658a359e9ff61f3fffe7875f73e
Nguyen Ho <mickey171904@gmail.com>
2026-04-14 21:18:41 -0700

    1st version of ClientService.java

--- src/main/java/com/HealthApp/service/ClientService.java
+++ src/main/java/com/HealthApp/service/ClientService.java
@@ -2,6 +2,8 @@ package com.HealthApp.service;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.HealthApp.model.Client;
 import com.HealthApp.model.Goal;
@@ -10,4 +12,31 @@ import com.HealthApp.repo.ClientRepository;
 @Service
 public class ClientService {
 
+    @Autowired
+    private ClientRepository repo;
+
+    public List<Client> getAllClients() {
+        return repo.findAll();
+    }
+
+    public Client getClientById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+    }
+
+    public void saveClient(Client client) {
+        repo.save(client);
+    }
+
+    public void deleteClient(Long id) {
+        repo.deleteById(id);
+    }
+
+    public long countClient() {
+        return repo.count();
+    }
+
+    public void deleteAllClients() {
+        repo.deleteAll();
+    }
 }
