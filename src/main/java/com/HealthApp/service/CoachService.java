6f969c2b77069d3c6bd6e1d55e0eb320fde9fcde
Nguyen Ho <mickey171904@gmail.com>
2026-04-14 21:29:22 -0700

    1st version of CoachService.java

--- src/main/java/com/HealthApp/service/CoachService.java
+++ src/main/java/com/HealthApp/service/CoachService.java
@@ -2,6 +2,8 @@ package com.HealthApp.service;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.HealthApp.model.Coach;
 import com.HealthApp.model.Specialty;
@@ -9,5 +11,32 @@ import com.HealthApp.repo.CoachRepository;
 
 @Service
 public class CoachService {
-	
+
+    @Autowired
+    private CoachRepository repo;
+
+    public List<Coach> findAllCoaches() {
+        return repo.findAll();
+    }
+
+    public Coach findCoachById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Coach not found"));
+    }
+
+    public void saveCoach (Coach coach) {
+        repo.save(coach);
+    }
+
+    public void deleteCoach(Long id) {
+        repo.deleteById(id);
+    }
+
+    public long countCoaches() {
+        return repo.count();
+    }
+
+    public void deleteAllCoaches() {
+        repo.deleteAll();
+    }
 }
