d77aadba2e8b9569eac6f3bb921f1dc854bff821
Nguyen Ho <mickey171904@gmail.com>
2026-04-15 14:41:27 -0700

    1st version of WorkoutService.java

--- src/main/java/com/HealthApp/service/WorkoutService.java
+++ src/main/java/com/HealthApp/service/WorkoutService.java
@@ -1,7 +1,40 @@
 package com.HealthApp.service;
 
+import com.HealthApp.model.Workout;
+import com.HealthApp.repo.WorkoutRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class WorkoutService {
 
+    @Autowired
+    private WorkoutRepository repo;
+
+    public List<Workout> getAllWorkout() {
+        return repo.findAll();
+    }
+
+    public Workout getWorkoutById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cannot find workout"));
+    }
+
+    public void saveWorkout (Workout workout) {
+        repo.save(workout);
+    }
+
+    public void deleteWorkout(Long id) {
+        repo.deleteById(id);
+    }
+
+    public long countWorkout() {
+        return repo.count();
+    }
+
+    public void deleteAllWorkout() {
+        repo.deleteAll();
+    }
 }
