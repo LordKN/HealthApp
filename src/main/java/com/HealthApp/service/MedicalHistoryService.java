6e7a72ec34fa740678540ebe403e27e43853d2e6
Nguyen Ho <mickey171904@gmail.com>
2026-04-15 14:30:27 -0700

    1st version of MedicalHistoryService.java

--- src/main/java/com/HealthApp/service/MedicalHistoryService.java
+++ src/main/java/com/HealthApp/service/MedicalHistoryService.java
@@ -1,7 +1,40 @@
 package com.HealthApp.service;
 
+import com.HealthApp.model.MedicalHistory;
+import com.HealthApp.repo.MedicalHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class MedicalHistoryService {
 
+    @Autowired
+    private MedicalHistoryRepository repo;
+
+    public List<MedicalHistory> getAllMedicalHistory() {
+        return repo.findAll();
+    }
+
+    public MedicalHistory getMedicalHistoryById (Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Medical History not found"));
+    }
+
+    public void saveMedicalHistory(MedicalHistory history) {
+        repo.save(history);
+    }
+
+    public void deleteMedicalHistory(Long id) {
+        repo.deleteById(id);
+    }
+
+    public long countMedicalHistory() {
+        return repo.count();
+    }
+
+    public void deleteAllMedicalHistory() {
+        repo.deleteAll();
+    }
 }
