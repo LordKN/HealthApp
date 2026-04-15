9b9c0786c33008c3559ce68b1994898ac6e4aeac
Nguyen Ho <mickey171904@gmail.com>
2026-04-14 21:01:21 -0700

    1st version of CertificateService.java

--- src/main/java/com/HealthApp/service/CertificateService.java
+++ src/main/java/com/HealthApp/service/CertificateService.java
@@ -1,7 +1,27 @@
 package com.HealthApp.service;
 
+import com.HealthApp.model.Certificate;
+import com.HealthApp.repo.CertificateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CertificateService {
 
+    @Autowired
+    private CertificateRepository repo;
+
+    public List<Certificate> getAllCertificates() {
+        return repo.findAll();
+    }
+
+    public Certificate getCertificateById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Certificate not found"));
+    }
 }
+
+//findById(id) will return an Optional object to represent a value that may or not may present,
+//  forcing developers to explicitly handle the absence of a value.
