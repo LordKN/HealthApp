423cf51fd3a738a43ca29e32e83d8a5fdc2faadb
Nguyen Ho <mickey171904@gmail.com>
2026-05-18 15:29:34 -0700

    Create some methods for CertificateController

--- src/main/java/com/HealthApp/controller/CertificateController.java
+++ src/main/java/com/HealthApp/controller/CertificateController.java
@@ -2,8 +2,7 @@ package com.HealthApp.controller;
 
 import com.HealthApp.model.Certificate;
 import com.HealthApp.service.CertificateService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,5 +14,32 @@ public class CertificateController {
     @GetMapping("/api/certificates")
     public List<Certificate> getAllCertificate() {
         return service.getAllCertificates();
+    }
+
+    @GetMapping("/api/certificate/{cerID}")
+    public Certificate getCertificate(@PathVariable("cerID") Long cerID) {
+        return service.getCertificateById(cerID);
+    }
+
+    @GetMapping("/api/certificates/count")
+    public Long countCertificate() {
+        return service.countCertificate();
+    }
+
+    @DeleteMapping("/api/certificate/{cerID}")
+    public String deleteCertificate(@PathVariable("cerID") Long cerID) {
+        service.deleteCertificate(cerID);
+        return "Deleted";
+    }
+
+    @DeleteMapping("/api/certificates")
+    public String deleteAllCertificate() {
+        service.deleteAllCertificate();
+        return "Deleted all certificate";
+    }
+
+    @PostMapping("/api/certificate")
+    public void saveCertificate(@RequestBody Certificate certificate) {
+        service.saveCertificate(certificate);
     }
 }
