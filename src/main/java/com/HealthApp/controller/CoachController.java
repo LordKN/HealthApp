7a2019abd44f089e972e6b1396076c2feeb654cc
Nguyen Ho <mickey171904@gmail.com>
2026-05-18 16:00:22 -0700

    Create some methods for CoachController

--- src/main/java/com/HealthApp/controller/CoachController.java
+++ src/main/java/com/HealthApp/controller/CoachController.java
@@ -3,8 +3,7 @@ package com.HealthApp.controller;
 import com.HealthApp.model.Coach;
 import com.HealthApp.service.CoachService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,32 @@ public class CoachController {
     @GetMapping("/api/coaches")
     public List<Coach> getAllCoaches() {
         return service.findAllCoaches();
+    }
+
+    @GetMapping("/api/coach/{coachId}")
+    public Coach getCoach(@PathVariable("coachId") Long id) {
+        return service.findCoachById(id);
+    }
+
+    @GetMapping("/api/coaches/count")
+    public Long countCoaches() {
+        return service.countCoaches();
+    }
+
+    @PostMapping("/api/coach")
+    public void saveCoach(@RequestBody Coach coach) {
+        service.saveCoach(coach);
+    }
+
+    @DeleteMapping("/api/coach/{coachID}")
+    public String deleteCoach(@PathVariable("coachID") Long id) {
+        service.deleteCoach(id);
+        return "Coach deleted";
+    }
+
+    @DeleteMapping("/api/coaches")
+    public String deleteAllCoaches() {
+        service.deleteAllCoaches();
+        return "All coaches deleted";
     }
 }
