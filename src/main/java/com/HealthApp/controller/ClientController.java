237dd9279bc4b307717c4b6c00c5c333c8fa1fb4
Nguyen Ho <mickey171904@gmail.com>
2026-05-18 15:40:47 -0700

    Create some methods for ClientController

--- src/main/java/com/HealthApp/controller/ClientController.java
+++ src/main/java/com/HealthApp/controller/ClientController.java
@@ -3,8 +3,7 @@ package com.HealthApp.controller;
 import com.HealthApp.model.Client;
 import com.HealthApp.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,32 @@ public class ClientController {
     @GetMapping("/api/clients")
     public List<Client> getAllClient() {
         return service.getAllClients();
+    }
+
+    @GetMapping("/api/client/{cliID}")
+    public Client getClient(@PathVariable("cliID") Long id) {
+        return service.getClientById(id);
+    }
+
+    @GetMapping("/api/clients/count")
+    public Long countClient() {
+        return service.countClient();
+    }
+
+    @PostMapping("/api/client")
+    public void saveClient(@RequestBody Client client) {
+        service.saveClient(client);
+    }
+
+    @DeleteMapping("/api/client/{cliID}")
+    public String deleteClient(@PathVariable("cliID") Long id) {
+        service.deleteClient(id);
+        return "Client Deleted";
+    }
+
+    @DeleteMapping("api/clients")
+    public String deleteAllClients() {
+        service.deleteAllClients();
+        return "All clients deleted";
     }
 }
