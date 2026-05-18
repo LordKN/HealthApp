1dfa3bfc922b0f64ec9b9650b6a78b7238bec7de
Nguyen <mickey171904@gmail.com>
2026-05-18 09:57:10 -0700

    add @RestController notation to CertificateController
    To tell Spring to return the value as HTTP response data

--- src/main/java/com/HealthApp/controller/CertificateController.java
+++ src/main/java/com/HealthApp/controller/CertificateController.java
@@ -4,9 +4,11 @@ import com.HealthApp.model.Certificate;
 import com.HealthApp.repo.CertificateRepository;
 import com.HealthApp.service.CertificateService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class CertificateController {
 
     private CertificateService service;
