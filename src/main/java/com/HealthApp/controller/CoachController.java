ecdf8ee8dbf3c27a7090684430a187d6290a181b
Nguyen <mickey171904@gmail.com>
2026-05-18 10:33:28 -0700

    CoachController created

---
+++ src/main/java/com/HealthApp/controller/CoachController.java
@@ -0,0 +1,19 @@
+package com.HealthApp.controller;
+
+import com.HealthApp.model.Coach;
+import com.HealthApp.service.CoachService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CoachController {
+
+    private CoachService service;
+
+    @GetMapping("/api/coaches")
+    public List<Coach> getAllCoaches() {
+        return service.findAllCoaches();
+    }
+}
