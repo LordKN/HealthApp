730f97f759fc2b704114a26b48eec1e44c4b1d81
Nguyen <mickey171904@gmail.com>
2026-05-18 10:04:31 -0700

    Add CertificateController

--- src/main/java/com/HealthApp/controller/CertificateController.java
+++ src/main/java/com/HealthApp/controller/CertificateController.java
@@ -1,7 +1,6 @@
 package com.HealthApp.controller;
 
 import com.HealthApp.model.Certificate;
-import com.HealthApp.repo.CertificateRepository;
 import com.HealthApp.service.CertificateService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
