ed1b7fae52c5a842831699fc6a252097a36a9fc3
Nguyen <mickey171904@gmail.com>
2026-06-02 16:07:55 +0700

    Correct routing for links in the navbar
    Correct routing for links in the navbar

--- src/main/java/com/HealthApp/controller/HomeController.java
+++ src/main/java/com/HealthApp/controller/HomeController.java
@@ -1,16 +1,27 @@
 package com.HealthApp.controller;
 
 import com.HealthApp.model.Certificate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/api/message")
-    public String getMessage() {
-        return "Hello from backend";
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "redirect:/html/about.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "redirect:/html/login.html";
     }
 }
