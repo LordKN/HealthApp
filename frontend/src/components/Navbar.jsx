2db378b085f9e2dc3d944bcb6a3e037ceb7cf863
Nguyen Ho <mickey171904@gmail.com>
2026-08-24 21:39:59 -0700

    fix: Fix routing problem when default open the home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import "../assets/css/navbar.css";
 export default function Navbar() {
   return (
     <div className="nav-pages">
-      <a href="/">Home</a>
+      <a href="/home">Home</a>
       <a href="/about">About Us</a>
       <a href="/login">Login</a>
     </div>
