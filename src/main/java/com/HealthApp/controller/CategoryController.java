494e8e7f676e14d086116e29099776e90d24b3bd
Nguyen Ho <mickey171904@gmail.com>
2026-09-02 10:45:10 -0700

    fix: Change controller annotations to @RestController

--- src/main/java/com/HealthApp/controller/CategoryController.java
+++ src/main/java/com/HealthApp/controller/CategoryController.java
@@ -5,13 +5,11 @@ import com.HealthApp.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CategoryController {
 
     @Autowired
