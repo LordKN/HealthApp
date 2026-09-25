972897e7d75ca76a4b92b4a14ccbfadb435405c0
Nguyen Ho <mickey171904@gmail.com>
2026-09-25 10:55:04 -0700

    style: add decoration symbols and restyle indentation with Prettier

--- frontend/src/pages/client/Sidebar.jsx
+++ frontend/src/pages/client/Sidebar.jsx
@@ -1,55 +1,51 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { logoutUser } from "../../services/api.js";
-import { useAuth } from "../../context/AuthContext.jsx"
+import { useAuth } from "../../context/AuthContext.jsx";
 import "../../assets/css/sidebar.css";
+import { House, Dumbbell } from "lucide-react";
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+  const { setAccessToken, setRole } = useAuth();
 
-    const navigate = useNavigate();
-    const { setAccessToken, setRole } = useAuth();
+  async function handleLogout() {
+    try {
+      await logoutUser();
 
-    async function handleLogout() {
-        try {
-            await logoutUser();
+      setAccessToken(null);
+      setRole(null);
 
-            setAccessToken(null);
-            setRole(null);
-
-            navigate("/login");
-        }   catch (error) {
-                console.error("Failed to logout", error);
-        }
+      navigate("/login");
+    } catch (error) {
+      console.error("Failed to logout", error);
     }
+  }
 
-    return (
-        <aside className = "sideboar">
-            <div>
-                <h1 className="sidebar-logo">
-                    Grind<span>Hub</span>
-                </h1>
+  return (
+    <aside className="sideboar">
+      <div>
+        <h1 className="sidebar-logo">
+          <Dumbbell size={20} />
+          Grind<span>Hub</span>
+        </h1>
 
-                <nav className = "sidebar-nav">
-                    <NavLink to="/client/dashboard">
-                        Dashboard 
-                    </NavLink>
+        <nav className="sidebar-nav">
+          <NavLink to="/client/dashboard">
+            <House size={15} />
+            Dashboard
+          </NavLink>
 
-                    <NavLink to="/client/exercises">
-                        Exercises
-                    </NavLink>
+          <NavLink to="/client/exercises">Exercises</NavLink>
 
-                    <NavLink to="/client/workouts">
-                        Workouts
-                    </NavLink>
+          <NavLink to="/client/workouts">Workouts</NavLink>
 
-                    <NavLink to="/client/profile">
-                        Profile
-                    </NavLink>
-                </nav>
-            </div>
+          <NavLink to="/client/profile">Profile</NavLink>
+        </nav>
+      </div>
 
-            <button className = "logout-button" onClick={handleLogout}>
-                Logout
-            </button>
-        </aside>
-    )
+      <button className="logout-button" onClick={handleLogout}>
+        Logout
+      </button>
+    </aside>
+  );
 }
