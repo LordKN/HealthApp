369745cbd830e31cd2ae7973ceb3c6fa8b6a8e9f
Nguyen Ho <mickey171904@gmail.com>
2026-09-25 10:33:57 -0700

    feat: Decorate dashboard sidebar with CSS files

---
+++ frontend/src/pages/client/Sidebar.jsx
@@ -0,0 +1,55 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { logoutUser } from "../../services/api.js";
+import { useAuth } from "../../context/AuthContext.jsx"
+import "../../assets/css/sidebar.css";
+
+export default function Sidebar() {
+
+    const navigate = useNavigate();
+    const { setAccessToken, setRole } = useAuth();
+
+    async function handleLogout() {
+        try {
+            await logoutUser();
+
+            setAccessToken(null);
+            setRole(null);
+
+            navigate("/login");
+        }   catch (error) {
+                console.error("Failed to logout", error);
+        }
+    }
+
+    return (
+        <aside className = "sideboar">
+            <div>
+                <h1 className="sidebar-logo">
+                    Grind<span>Hub</span>
+                </h1>
+
+                <nav className = "sidebar-nav">
+                    <NavLink to="/client/dashboard">
+                        Dashboard 
+                    </NavLink>
+
+                    <NavLink to="/client/exercises">
+                        Exercises
+                    </NavLink>
+
+                    <NavLink to="/client/workouts">
+                        Workouts
+                    </NavLink>
+
+                    <NavLink to="/client/profile">
+                        Profile
+                    </NavLink>
+                </nav>
+            </div>
+
+            <button className = "logout-button" onClick={handleLogout}>
+                Logout
+            </button>
+        </aside>
+    )
+}
